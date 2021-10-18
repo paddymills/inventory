7aa413d8e096c0f18cd77e712a7b690637940c58
paddymills <paddymills@pm.me>
2021-10-18 12:29:59 -0400

    in process query add mm

--- src/in_process.py
+++ src/in_process.py
@@ -4,11 +4,18 @@ from lib import sndb
 with sndb.get_sndb_conn() as db:
     cursor = db.cursor()
     cursor.execute("""
-        SELECT *
+        SELECT
+            CompletedProgram.AutoID, CompletedProgram.CompletedDateTime,
+            CompletedProgram.ProgramName,
+            Program.SheetName, Stock.PrimeCode
         FROM CompletedProgram
+        INNER JOIN Program
+            ON CompletedProgram.ProgramName=Program.ProgramName
+        INNER JOIN Stock
+            ON Program.SheetName=Stock.SheetName
         WHERE CompletedDateTime > '2021-10-18 04:00:00'
     """)
 
     for row in cursor.fetchall():
         dt = row.CompletedDateTime.isoformat()
-        print('{} | {}\t{}\t{}'.format(row.AutoID, row.ProgramName, row.SheetName, dt))
+        print('[{}] {} | {}\t{}\t{}'.format(dt, row.AutoID, row.ProgramName, row.SheetName, row.PrimeCode))
