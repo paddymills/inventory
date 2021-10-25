c09af41c28c047830c025c517cffc88b43efe299
paddymills <paddymills@pm.me>
2021-10-25 08:03:00 -0400

    modularize and add note

--- src/recd_when.py
+++ src/recd_when.py
@@ -2,23 +2,38 @@
 from lib import sndb
 import xlwings
 
-with sndb.get_sndb_conn() as db:
-    cursor = db.cursor()
-    cursor.execute("""
-        SELECT *
-        FROM Stock
-        INNER JOIN TransActLog
-            ON Stock.SheetName=TransActLog.ItemName
-        WHERE TransActLog.PrimeCode = '50/50W-0008'
-    """)
 
-    data = [['Date Received', 'SheetName', 'Location']]
-    for row in cursor.fetchall():
-        data.append([
-            row.DateReceived,
-            row.SheetName,
-            row.Location,
-        ])
+def mm_recd(mm):
+    """
+    
+    gets all plates from SimTrans log
+    that match a given material master
+    and dumps it to a new excel workbook.
 
-    wb = xlwings.Book()
-    wb.sheets.active.range("A1").value = data
+    """
+
+
+    with sndb.get_sndb_conn() as db:
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT *
+            FROM Stock
+            INNER JOIN TransActLog
+                ON Stock.SheetName=TransActLog.ItemName
+            WHERE TransActLog.PrimeCode = ?
+        """, mm)
+
+        data = [['Date Received', 'SheetName', 'Location']]
+        for row in cursor.fetchall():
+            data.append([
+                row.DateReceived,
+                row.SheetName,
+                row.Location,
+            ])
+
+        wb = xlwings.Book()
+        wb.sheets.active.range("A1").value = data
+
+
+if __name__ == "__main__":
+    mm_recd('50/50W-0008')
