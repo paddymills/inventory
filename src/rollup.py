34683e5a2a0def05fa1610a123fe84c71294ac89
paddymills <paddymills@pm.me>
2021-11-05 13:23:09 -0400

    add compare

--- src/rollup.py
+++ src/rollup.py
@@ -8,11 +8,17 @@ LOCATION_RE = regex(r'([A-Za-z])(\d+)')
 
 
 def main():
+    dmain = main_yard()
+    dsecondary = detail_bay()
+
     dump_to_xl([
-        dict(name="Plant 2 Yard",       data=main_yard()),
-        dict(name="Detail Bay Yard",    data=detail_bay()),
-        # dict(name="Staged or Burned",   data=staged_or_burned()),
+        # dict(name="Plant 2 Yard",       data=dmain),
+        # dict(name="Detail Bay Yard",    data=dsecondary),
+        dict(name="SigmaNest",          data=[*dmain, *dsecondary])
     ])
+
+    if "export.XLSX" in [b.name for b in xlwings.books]:
+        do_compare()
 
 
 def main_yard():
@@ -87,7 +93,7 @@ def pull_locs(locs):
             for row in cursor.fetchall():
                 key = "{}_{}_{}".format(row.loc, row.mm, row.wbs)
                 if key not in data:
-                    data[key] = [key, row.loc, row.mm, row.wbs, 0, row.uom]
+                    data[key] = [None, row.loc, row.mm, row.wbs, 0, row.uom]
 
                 data[key][4] += row.area
 
@@ -106,8 +112,42 @@ def dump_to_xl(datasets):
         sheet.name = data["name"]
         sheet.range("A1").value = ["UID", "Location", "SAP MM", "WBS Element", "Qty", "UoM"]
         sheet.range("A2").value = data["data"]
+        for r in range(2, len(data["data"]) + 2):
+            sheet.range(r, 1).formula = '=CONCATENATE(B{0},"_",C{0},"_",D{0})'.format(r)
         sheet.range("D:D").number_format = '0.000'
         sheet.autofit()
+
+
+def do_compare():
+    wb = xlwings.books.active
+    xlwings.books["export.XLSX"].sheets[0].copy(after=wb.sheets[-1], name="SAP")
+    
+    sheet = wb.sheets["SAP"]
+    sheet.range("A:A").insert(shift='right')
+    sheet.range("A1").value = "UID"
+
+    quantifiers = wb.sheets["SigmaNest"].range("B2:D2").expand('down').value
+    for r in range(2, sheet.range("B2").expand('down').end('down').row + 1):
+        if not sheet.range(r, 2).value:
+            break
+
+        sheet.range(r, 1).formula = '=CONCATENATE(E{0},"_",B{0},"_",I{0})'.format(r)
+        quantifiers.append([sheet.range(r, x).value for x in (5, 2, 9)])
+
+    sheet = wb.sheets.add("Compare")
+    sheet.range("A1").value = ["UID", "SLoc Sort", "Location", "SAP MM", "WBS Element", "SigmaNest", "SAP", "SAP UoM", "Adj SAP Qty", "SAP/Sigmanest Δ"]
+    for r, row in enumerate(sorted(set([tuple(q) for q in quantifiers]), key = lambda x: x[1]), start=2):
+        sheet.range(r, 3).value = row
+        sheet.range(r, 1).formula = '=CONCATENATE(C{0},"_",D{0},"_",E{0})'.format(r)
+        sheet.range(r, 2).formula = '=CONCATENATE(LEFT(C{0},1),TEXT(MID(C{0},2,LEN(C{0})-1),"00"))'.format(r)
+        sheet.range(r, 6).formula = '=VLOOKUP(A{0},SigmaNest!A:E,5,FALSE)'.format(r)
+        sheet.range(r, 7).formula = '=VLOOKUP(A{0},SAP!A:F,6,FALSE)'.format(r)
+        sheet.range(r, 8).formula = '=VLOOKUP(A{0},SAP!A:G,7,FALSE)'.format(r)
+        sheet.range(r, 9).formula = '=IFERROR(IF(H{0}="IN2",G{0},G{0}*144),0)'.format(r)
+        sheet.range(r, 10).formula = '=I{0}-F{0}'.format(r)
+
+    sheet.autofit()
+    sheet.range("J:J").number_format = "0,000.00"
 
 
 if __name__ == "__main__":
