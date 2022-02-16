9a6cf919b938f99d2fe15cb82c4bd64db8b7b4af
paddymills <paddymills@pm.me>
2022-02-16 12:38:59 -0500

    extend capabilities

--- src/build_failures.py
+++ src/build_failures.py
@@ -1,4 +1,7 @@
 
+import pyautogui
+import time
+
 from argparse import ArgumentParser
 from datetime import datetime
 from os import path, listdir
@@ -9,24 +12,48 @@ base = r"\\hssieng\SNData\SimTrans\SAP Data Files"
 timestamp = "Production_{:%Y%m%d%H%M%S}.ready".format(datetime.now())
 output_filename = path.join(base, "other", timestamp)
 
-NUM_FILES = 200
+NUM_FILES = 500
+
+# regular expressions
+INBOX_TEXT_1 = regex(r"Planned order not found for (\d{7}[a-zA-Z])-([\w-]+), [\w-]*, ([\d,.]+), Sigmanest Program:(\d+)")
 JOB_RE = regex(r"\d{7}[a-zA-Z]")
+JOB_MARK_RE = regex(r"\d{7}[a-zA-Z]-[a-zA-Z0-9-]+")
 WBS_RE = regex(r"(?:[SD]-)?(?:\d{7}?-)?(\d{5})")
 PROG_RE = regex(r"\d{5}")
+SPLIT_RE = regex(r"[\t, ]")
+
+# looping offsets
+LOOP_DISPLAY = (2700, 170)
+LOOP_EXPAND = (1956, 625)
+LOOP_BACK = (2210, 60)
+LOOP_DELAY = 1.0
+
+class FoundState:
+
+    FULL_MATCH      = 1
+    WITHOUT_WBS     = 2
+    WITHOUT_PROG    = 3
+    NONE            = 4
 
 class FailuresFinder:
 
     def __init__(self):
         ap = ArgumentParser()
         ap.add_argument("--txt", action="store_true", help="read from text file")
+        ap.add_argument("--nowbs", action="store_true", help="no wbs option for read from text file")
+        ap.add_argument("--loop", action="store_true", help="loop to get input (adjust offsets first please)")
         ap.add_argument("--all", action="store_true", help="process all files")
 
         self.args = ap.parse_args()
 
-        self.find_job   = None
-        self.find_mark  = None
-        self.find_wbs   = None
-        self.find_prog  = None
+        self.job   = None
+        self.mark  = None
+        self._wbs   = None
+        self.prog  = None
+
+        self.in2 = None
+
+        self.found = FoundState.NONE
 
     @property
     def files(self):
@@ -36,7 +63,24 @@ class FailuresFinder:
 
         return _files[:NUM_FILES]
 
+    @property
+    def wbs(self):
+        if self._wbs:
+            return "D-{}-{}".format(self.job[:-1], self._wbs)
+
+        return None
+
+    @property
+    def part(self):
+        return "{}-{}".format(self.job, self.mark)
+
+    def swap_wbs_and_program(self):
+        self._wbs, self.prog = self.prog, self._wbs
+
     def run(self):
+        if self.args.loop:
+            return self.loop_input()
+
         if self.args.txt:
             return self.from_txt()
 
@@ -63,54 +107,141 @@ class FailuresFinder:
         print("file placed at: {}".format(output_filename))
         input("press any key to end")
 
+    def loop_input(self):
+        print("Enter args in order -> job mark wbs program")
+        print("===================================================")
+
+        while 1:
+            pyautogui.click(*LOOP_DISPLAY)
+            time.sleep(LOOP_DELAY)
+            pyautogui.click(*LOOP_EXPAND)
+            pyautogui.hotkey('alt', 'tab')
+
+            val = input("> ")
+            if not val:
+                break
+
+            try:
+                iters = 0
+                while 1:
+                    if iters == 1:
+                        self.swap_wbs_and_program()
+                    elif iters > 1:
+                        self.swap_wbs_and_program()
+                        val = input("\t> ")
+                        if not val:
+                            break
+
+                    if iters != 1:
+                        self.parse_args(val)
+                    
+                    if self.find_data():
+                        break
+
+                    iters += 1
+
+
+            except RuntimeError as e:
+                print(e)
+
+            pyautogui.click(*LOOP_BACK)
+            time.sleep(LOOP_DELAY)
+            pyautogui.press('down')
+            time.sleep(LOOP_DELAY)
+
+        print("file placed at: {}".format(output_filename))
+
     def from_txt(self):
         with open("input.txt", "r") as input_txt:
             for line in input_txt.readlines():
                 try:
                     self.parse_args(line)
-                except RuntimeError as e:
+                except RuntimeError:
                     print("Error parsing line:", line)
                     return
 
                 self.find_data()
 
     def parse_args(self, argstr):
-        found_wbs = False
+        argstr = argstr.strip()
 
-        for arg in argstr.upper().split(" "):
-                if JOB_RE.match(arg):
-                    self.find_job = arg
+        if INBOX_TEXT_1.match(argstr):
+            self.job, self.mark, self.in2, self.prog = INBOX_TEXT_1.match(argstr).groups()
+            self.in2 = self.in2.replace(",", "")
+            return
 
-                elif WBS_RE.match(arg) and not found_wbs:
-                    self.find_wbs = WBS_RE.match(arg).group(1)
-                    found_wbs = True
+        self.in2 = None
+        for arg in SPLIT_RE.split(argstr.upper()):
+            if JOB_MARK_RE.match(arg):
+                self.job, self.mark = arg.split("-", 1)
 
-                elif PROG_RE.match(arg):
-                    self.find_prog = arg
+            elif JOB_RE.match(arg):
+                self.job = arg
 
-                else:   # part
-                    self.find_mark = arg
+            elif arg.startswith('5') and PROG_RE.match(arg):
+                self.prog = arg
 
-        if None in self.find_vals:
+            elif WBS_RE.match(arg):
+                self._wbs = WBS_RE.match(arg).group(1)
+                found_wbs = True
+
+            else:   # part
+                self.mark = arg
+
+        if None in (self.job, self.prog, self.mark) and (not self.args.nowbs and self._wbs is None):
             raise RuntimeError("Not all arguments were provided")
 
-        return self.find_vals
-
     def find_data(self):
-        part = "{}-{}".format(self.job, self.mark)
-        wbs = "D-{}-{}".format(self.job[:-1], self.wbs)
-
-        for f in tqdm(self.files, desc="Finding {}".format((part, wbs, self.find_prog)), leave=False):
+        self.found = FoundState.NONE
+        for f in tqdm(self.files, desc="Finding {}".format((self.part, self.wbs, self.prog)), leave=False):
             with open(path.join(base, "deleted files", f), 'r') as prod_file:
                 for row in prod_file.readlines():
-                    vals = row.split('\t')
-                    if vals[0].upper() == part and vals[2] == wbs and vals[12] == self.find_prog:
+                    if self.row_is_match(row):
                         with open(output_filename, 'a') as res_file:
                             res_file.write(row)
 
-                        return
+                        if self.found == FoundState.FULL_MATCH:
+                            return True
+
+                # break if found and not searching without program
+                if self.found == FoundState.WITHOUT_WBS:
+                            return True
+
+        if self.found == FoundState.NONE:
+            print("Row not found", (self.part, self.wbs, self.prog))
+            return False
+        
+        return True
+
+    def row_is_match(self, row):
+        # checks if row is a match
+        # returns match type, which determines loop exit
+
+        vals = row.split('\t')
+        if vals[0].upper() != self.part:
+            return False
+        if self.wbs and vals[2] != self.wbs:
+            return False
+        if self.prog and vals[12] != self.prog:
+            return False
+
+        # if in2 is supplied (parsed from inbox text), skip when mismatched in2
+        if self.in2 and vals[8] != self.in2:
+            return False
+
+        # if we made it this far, then we can assume:
+        #   - part is a match
+        #   - wbs is a match or searching without a wbs
+        #   - program is a match or searching without a program
+        if self.prog is None:
+            self.found = FoundState.WITHOUT_PROG
+        elif self.wbs is None:
+            self.found = FoundState.WITHOUT_WBS
         else:
-            print("Row not found", (part, wbs, self.find_prog))
+            self.found = FoundState.FULL_MATCH
+
+        
+        return True
 
 
 if __name__ == "__main__":
