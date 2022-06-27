35b947b16d9993bcf94d85bed8da3843bd082957
paddymills <paddymills@pm.me>
2022-06-27 09:26:38 -0400

    clean up main

--- src/bom.py
+++ src/bom.py
@@ -26,15 +26,15 @@ def main():
     parser.add_argument("shipment", nargs="?", default=None)
     args = parser.parse_args()
 
-    if args.verbose > 1:
-        print(args)
-
     if "-" in args.job:
         assert args.shipment is None, "Shipment must be specified in either job or shipment arguments"
         args.job, args.shipment = args.job.split("-")
 
     if args.fix_workorder:
         args.no_stock = True
+
+    if args.verbose > 1:
+        print(args)
 
     parts = list()
     with db.DbConnection(server='HSSSQLSERV', use_win_auth=True) as conn:
@@ -53,75 +53,78 @@ def main():
                 print(p)
 
     if args.fix_workorder:
-        wo_pattern = "{}-{}-%".format(args.job, args.shipment)
-        with db.SndbConnection() as conn:
-            for p in parts:
-                sn_part = "{}_{}".format(args.job, p.mark)
-                conn.cursor.execute(
-                    "SELECT * FROM Part WHERE PartName=? AND WONumber LIKE ?",
-                    (sn_part, wo_pattern)
-                )
-                if conn.cursor.fetchone() is None:
-                    conn.cursor.execute(
-                        """
-                            INSERT INTO TransAct (
-                                TransType, District, OrderNo, ItemName, Qty, DwgNumber, Material,
-                                Remark, ItemData1, ItemData2, ItemData5, ItemData6, ItemData7, ItemData8, ItemData9, ItemData12, ItemData14
-                            )
-                            SELECT
-                                'SN84' AS TransType,
-                                2 AS District,
-                                REPLACE(WONumber, CONCAT('-', Data2, '-'), CONCAT('-', ?, '-')) AS WONumber,
-                                PartName,
-                                ? AS QtyOrdered,
-                                DrawingNumber,
-                                Material,
-                                Remark,
-                                ? AS Data1,
-                                ? AS Data2,
-                                (
-                                    SELECT TOP 1 Data5 AS ChargeRef
-                                    FROM Part
-                                    WHERE WONumber LIKE ?
-                                ) AS Data5,
-                                Data6, Data7, Data8,
-                                Data9, Data12,
-                                'HighHeatNum' AS Data14
-                            FROM (
-                                SELECT
-                                    WONumber, PartName, DrawingNumber, Material, Remark, Data2, Data6, Data7, Data8, Data9, Data12
-                                FROM Part
-
-                                UNION
-
-                                SELECT
-                                    WONumber, PartName, DrawingNumber, Material, Remark, Data2, Data6, Data7, Data8, Data9, Data12
-                                FROM PartArchive
-                            ) AS Part
-                            WHERE WONumber LIKE ? AND PartName=?
-                        """,
-                        (
-                            args.shipment,
-                            p.qty, args.job, args.shipment,
-                            wo_pattern,
-                            wo_pattern.replace("-%s-" % args.shipment, "-%-"),
-                            sn_part
-                        )
-                    )
-
-                else:
-                    conn.cursor.execute(
-                        """
-                            UPDATE Part SET QtyOrdered=?
-                            WHERE PartName=? AND WONumber LIKE ?
-                        """,
-                        (p.qty, sn_part, wo_pattern)
-                    )
-
-            conn.commit()
+        fix_workorder(args.job, args.shipment, parts)
     else:
         dump_to_xl(parts, args.job, args.shipment)
 
+
+def fix_workorder(job, shipment, parts):
+    wo_pattern = "{}-{}-%".format(job, shipment)
+    with db.SndbConnection() as conn:
+        for p in parts:
+            sn_part = "{}_{}".format(job, p.mark)
+            conn.cursor.execute(
+                "SELECT * FROM Part WHERE PartName=? AND WONumber LIKE ?",
+                (sn_part, wo_pattern)
+            )
+            if conn.cursor.fetchone() is None:
+                conn.cursor.execute(
+                    """
+                        INSERT INTO TransAct (
+                            TransType, District, OrderNo, ItemName, Qty, DwgNumber, Material,
+                            Remark, ItemData1, ItemData2, ItemData5, ItemData6, ItemData7, ItemData8, ItemData9, ItemData12, ItemData14
+                        )
+                        SELECT
+                            'SN84' AS TransType,
+                            2 AS District,
+                            REPLACE(WONumber, CONCAT('-', Data2, '-'), CONCAT('-', ?, '-')) AS WONumber,
+                            PartName,
+                            ? AS QtyOrdered,
+                            DrawingNumber,
+                            Material,
+                            Remark,
+                            ? AS Data1,
+                            ? AS Data2,
+                            (
+                                SELECT TOP 1 Data5 AS ChargeRef
+                                FROM Part
+                                WHERE WONumber LIKE ?
+                            ) AS Data5,
+                            Data6, Data7, Data8,
+                            Data9, Data12,
+                            'HighHeatNum' AS Data14
+                        FROM (
+                            SELECT
+                                WONumber, PartName, DrawingNumber, Material, Remark, Data2, Data6, Data7, Data8, Data9, Data12
+                            FROM Part
+
+                            UNION
+
+                            SELECT
+                                WONumber, PartName, DrawingNumber, Material, Remark, Data2, Data6, Data7, Data8, Data9, Data12
+                            FROM PartArchive
+                        ) AS Part
+                        WHERE WONumber LIKE ? AND PartName=?
+                    """,
+                    (
+                        shipment,
+                        p.qty, job, shipment,
+                        wo_pattern,
+                        wo_pattern.replace("-%s-" % shipment, "-%-"),
+                        sn_part
+                    )
+                )
+
+            else:
+                conn.cursor.execute(
+                    """
+                        UPDATE Part SET QtyOrdered=?
+                        WHERE PartName=? AND WONumber LIKE ?
+                    """,
+                    (p.qty, sn_part, wo_pattern)
+                )
+
+        conn.commit()
 
 def dump_to_xl(data, job, shipment):
     wb = xlwings.Book(XML_PARTS_XLS)
