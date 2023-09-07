869d7d424ea633a73f63fccfb64ac9dcd0c3d52a
Patrick Miller <paddymills@proton.me>
2023-09-07 15:37:11 -0400

    add work order file calc

--- src/lib/paths.py
+++ src/lib/paths.py
@@ -1,3 +1,17 @@
 
+import os
+
 SAP_SIGMANEST_PRD = r"\\hiifileserv1\sigmanestprd"
 SAP_DATA_FILES = r"\\hssieng\SNData\SimTrans\SAP Data Files"
+SIGMANEST_WORKORDERS = r"\\hssieng\DATA\HS\SAP - Material Master_BOM\SigmaNest Work Orders"
+
+def workorder_file(job, shipment, extra_id=None):
+    extra = ''
+    if extra_id:
+        extra = '_{}'.format(extra_id)
+
+    return os.path.join(
+        SIGMANEST_WORKORDERS,
+        "20{} WOrk Orders Created".format(job[1:3]),
+        "{}-{}_SimTrans_WO{}.xls".format(job, shipment, extra)
+    )
